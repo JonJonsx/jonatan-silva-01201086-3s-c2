3226944ee7d29d0f13dcf09d765082f512601f14
JonJonsx <jonatan.souza@bandtec.com.br>
2021-04-16 17:36:51 -0300

    Questão B concluida

--- luta-livre/luta-livre/src/main/java/br/com/jonatan/lutalivre/controle/LutadorController.java
+++ luta-livre/luta-livre/src/main/java/br/com/jonatan/lutalivre/controle/LutadorController.java
@@ -4,12 +4,10 @@ import br.com.jonatan.lutalivre.dominio.Lutador;
 import br.com.jonatan.lutalivre.respositorio.LutadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lutadores")
@@ -23,4 +21,8 @@ public class LutadorController {
     repository.save(novoLutador);
     return ResponseEntity.status(200).build();
   }
+
+  @GetMapping
+  public List<Lutador> getLutador(){ return repository.findAll(); }
+
 }
